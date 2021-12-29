537f2f98a6dcbb1e172f499d73b0387ea3635234
hernandezvargash <hernandezvargas@outlook.com>
2021-12-29 18:20:50 +0100

    add runs list

---
+++ list_of_sequencing_runs.R
@@ -0,0 +1,73 @@
+
+#######################################################
+
+# 190721
+# Treg ID under DMK exposure
+# Maria/Valerie/NaomiChloe/Julien
+# cas9 targeting data
+
+#######################################################
+
+20190823
+Ligation sequencing kit
+Cas9 enrichment protocol for immune pannel; olivier colab; Mouse spleen DNA
+
+20200826
+Crispr targeted LIGATION sequencing SQK-LSK109
+Cas9 enrichment for immune identity panel test for 1000ng using Valeries DNA 
+
+20200907and  20201008
+Crispr targeted LIGATION sequencing SQK-LSK109
+Cas9 enrichment for immune identity panel Sample ID CAR152 DMK (pooled samples 10 and 11) Valerie AND second run: UNCALLED: HepaRG differentiated cells (D4W0.2) enriched for Chr20
+
+#20200910
+Crispr targeted LIGATION sequencing SQK-LSK109
+Cas9 enrichment for immune identity panel Sample ID CAR152 TR (pooled samples 7 and 8) Valerie
+
+20201207
+Crispr targeted LIGATION sequencing SQK-LSK109
+EXP-NBD104
+RB1=ID28, RB2=ID31, RB3=ID34, RB4=ID37
+Imune identity panel + UNCALLED samples from Valeries Mice (second set of samples sent) 
+
+#20201210
+Crispr targeted LIGATION sequencing SQK-LSK109
+EXP-NBD104
+RB5=ID43, RB6=ID46, RB7=ID49
+Imune identity panel samples from Valeries Mice (second set of samples sent) 
+
+#20201215
+Crispr targeted LIGATION sequencing SQK-LSK109
+EXP-NBD104
+RB8=ID29, RB9=ID32, RB10=ID40
+Imune identity panel samples from Valeries Mice (second set of samples sent) 
+
+#20210113
+Crispr targeted LIGATION sequencing SQK-LSK109
+EXP-NBD104
+RB1=Th0, RB2=Th1, RB3=Th2, RB4=TH17
+Imune identity panel samples from Valeries Mice (second set of samples sent) Replicates from the AK28 set
+
+#20210312
+Crispr targeted LIGATION sequencing SQK-LSK109
+EXP-NBD104
+RB1=ID66,Th0, RB2=ID69,Th1, RB3=ID72,Th2, RB4=ID77,TH17
+Cas9 enrichment of Imune identity panel samples from Valeries Mice (second set of samples sent) Replicates from the AKXX set
+
+#20210317
+Crispr targeted LIGATION sequencing SQK-LSK109
+EXP-NBD104
+B01=Th0 (ID84)
+B02=Th1 (ID86)
+B03=Th2 (ID87) 
+B04=Th17 (ID88) 
+Imune identity panel samples from Valeries Mice (second set of samples sent) Replicates from the ARJP28 set
+
+#20210324
+Crispr targeted LIGATION sequencing SQK-LSK109
+EXP-NBD104
+RB11: Th17nTGFB RB12: Th17nTGFB 
+Cas9 enrichment of Immune identity panel samples 
+
+#######################################################
+
